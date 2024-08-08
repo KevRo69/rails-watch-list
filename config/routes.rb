0dd0b13920548640fe54273048a032725220f7b0
kevin rossi <kevinrossi@free.fr>
2024-08-08 12:30:19 +0200

    List controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :lists, only: [:index, :show, :new, :create]
 end
